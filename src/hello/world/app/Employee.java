c8503834a45633d4493e4febb3ecbec724f3e05a
JagmeetGurm <jagmeetgurm@yahoo.com>
2015-07-08 23:32:34 -0700

    listing all files in a directory

--- src/hello/world/app/Employee.java
+++ src/hello/world/app/Employee.java
@@ -345,6 +345,28 @@ FileReader in2 = null;
       File d = new File(dirname);
       // Create directory now.
       d.mkdirs();
+      
+      
+      File file = null;
+      String[] paths;
+            
+      try{      
+         // create new file object
+         file = new File("\\tmp");
+                                 
+         // array of files and directory
+         paths = file.list();
+            
+         // for each name in the path array
+         for(String path:paths)
+         {
+            // prints filename and directory name
+            System.out.println(path);
+         }
+      }catch(Exception e){
+         // if any error occurs
+         e.printStackTrace();
+      }
 }
 
 }
